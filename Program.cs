967b5aecaef0b9e500ad5abb91cb3d6096aebb22
kalpanabolli11@gmail.com <kalpanabolli11@gmail.com>
2023-07-25 11:46:38 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace ConAppStudentData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string filePath = "D:\\Simply Learn\\Student Data.txt";
+
+                if (File.Exists(filePath))
+                {
+                    string[] lines = File.ReadAllLines(filePath);
+
+                    foreach (string line in lines)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The student data file does not exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred: " + ex.Message);
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
